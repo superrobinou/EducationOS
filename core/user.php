8aa437a67df619413acd49341c4aaabd13e8ddd2
robin thomas <superrobinou@users.noreply.github.com>
2016-11-08 16:49:46 +0100

    Add files via upload

---
+++ core/user.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author rthomas
+ *
+ */
+class User{
+	var $idUser;
+	var $name;
+	var $firstname;
+	var $motdepasse;
+	var $email;
+	function User($idUser,$name,$firsname,$password,$mail){
+		$this->idUser=$idUser;
+		$this->name=$name;
+		$this->firstname=$firsname;
+		$this->motdepasse=$password;
+		$this->email=$mail;
+	}
+	function Connect($idUser,$password){
+	}
+	function Disconnect($idUser){
+	}
+	function ChangeInformations($name,$firsname,$password=null,$password2=null,$mail=null){
+		if($password<>null){
+			if($password2=password){
+				
+			}
+		else if($email<>null){
+			
+		}
+		}
+	}
+	function changepasseword($idUser,$password,$password2){
+		
+	}
+	function changeemail($idUser,$password,$password2){
+		
+	}
+}
